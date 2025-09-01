b04b70f364f1556396c6dde92764e7a361aa8071
Markus Fritze <github@sarnau.com>
2025-09-01 18:09:53 +0200

    Update HP3458A.py

--- HP3458A.py
+++ HP3458A.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import time
+from datetime import datetime
 import pyvisa
 
 # send a command and read the response
@@ -33,30 +34,33 @@ def nvram_read(fname):
 	l = mread(nvram_base, nvram_base + nvram_size * 2)
 	open(fname, "wb").write(l)
 
+rm = pyvisa.ResourceManager()
+dev = rm.open_resource('TCPIP::192.168.1.192::gpib0,22::INSTR')
 while True:
-	rm = pyvisa.ResourceManager()
-	dev = rm.open_resource('TCPIP::192.168.1.192::gpib0,22::INSTR')
-	dev.write("QFORMAT ALPHA")
-	if False:
-		print(query("ID?"))
-		print(query("REV?"))
-		print(query("OPT?"))
-		print(query("DSP?"))
-		print(query("CAT"))
-	print(query("TEMP?"))
-	if False:
-		for i in range(10):
-			print(query("DEFKEY? %d" % i))
-	if False:
-		print(query("CALSTR?"))
-		print(query("CALNUM?"))
-		for i in [2941,2437]:
-			print(query("CAL? %d" % i))
-	if False:
-		for i in range(1,254):
-			for l in [0,1,3,5]:
-				print(query("CAL? %d,%d" % (i,l)))
-
+	print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+	try:
+		dev.write("QFORMAT ALPHA")
+		#print(query("DSP?"))
+		#print(query("ID?"))
+		#print(query("REV?"))
+		#print(query("OPT?"))
+		#print(query("CAT"))
+		print(query("TEMP?"))
+		if False:
+			for i in range(10):
+				print(query("DEFKEY? %d" % i))
+		if False:
+			print(query("CALSTR?"))
+			print(query("CALNUM?"))
+			for i in [2941,2437]:
+				print(query("CAL? %d" % i))
+		if False:
+			for i in range(1,254):
+				for l in [0,1,3,5]:
+					print(query("CAL? %d,%d" % (i,l)))
+	
 #	print(query("TARM SGL,1"))
 #	nvram_read("hp3458.calram.bin")
-	time.sleep(60)
+	except:
+		pass
+	time.sleep(10)
